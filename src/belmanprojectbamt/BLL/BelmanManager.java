e84c37e48d796e3fa72a5ee859cd22f9b5922995
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-04-24 12:13:26 +0200

    Program skeleton

---
+++ src/belmanprojectbamt/BLL/BelmanManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belmanprojectbamt.BLL;
+
+/**
+ *
+ * @author Theis
+ */
+public class BelmanManager implements LogicInterface
+{
+
+    public BelmanManager()
+    {
+     
+    }
+    
+}
---
+++ src/belmanprojectbamt/DAL/Facade.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belmanprojectbamt.DAL;
+
+/**
+ *
+ * @author Theis
+ */
+public class Facade implements IFacade
+{
+    
+}
