03aaaf411bbe563a371617120b2003fe4e2913e6
Asvør <asvor_rasmussen@hotmail.com>
2019-04-26 10:22:45 +0100

    scrollpane added

--- src/belmanprojectbamt/GUI/Controller/FXMLDocumentController.java
+++ src/belmanprojectbamt/GUI/Controller/FXMLDocumentController.java
@@ -27,6 +27,8 @@ public class FXMLDocumentController implements Initializable
     PostItFactory pFactory;
     @FXML
     private AnchorPane slider;
+    @FXML
+    private AnchorPane ancPane;
     /**
      * Initializes the controller class.
      * @param url
@@ -41,6 +43,9 @@ public class FXMLDocumentController implements Initializable
         flowPane.setVgap(5);
         flowPane.setHgap(5);
         flowPane.setOrientation(Orientation.VERTICAL);
+        
+//        ancPane.layoutXProperty().bi(flowPane.layoutXProperty());
+//        flowPane.layoutXProperty().bindBidirectional(ancPane.layoutXProperty());
     }    
     
     public void generatePostIt()
