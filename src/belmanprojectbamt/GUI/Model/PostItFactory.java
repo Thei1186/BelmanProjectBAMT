c5a46982ea010a28a82e03e14a27eebe6fd561f3
Asvør <asvor_rasmussen@hotmail.com>
2019-05-14 09:21:34 +0100

    new design

--- src/belmanprojectbamt/GUI/Model/PostItFactory.java
+++ src/belmanprojectbamt/GUI/Model/PostItFactory.java
@@ -8,14 +8,17 @@ package belmanprojectbamt.GUI.Model;
 import belmanprojectbamt.BE.DepartmentTask;
 import belmanprojectbamt.BE.Order;
 import java.util.List;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 /**
@@ -50,9 +53,10 @@ public class PostItFactory
             setProgressBar();
             createAnchorPane();
             createLabels();
-            createComboBox();
+//            createComboBox();
             createButton(index);
             setLabelText(index);
+            createVBox();
         }
         index++;
 
@@ -62,19 +66,19 @@ public class PostItFactory
     private void setProgressBar()
     {
         ProgressBar prgBarDate = new ProgressBar();
-        ProgressBar prgBarActual = new ProgressBar();
+//        ProgressBar prgBarActual = new ProgressBar();
 
         prgBarDate.setLayoutX(14);
-        prgBarDate.setLayoutY(240);
+        prgBarDate.setLayoutY(305);
         prgBarDate.setProgress(0.4);
         prgBarDate.setPrefSize(565, 24);
 
-        prgBarActual.setLayoutX(14);
-        prgBarActual.setLayoutY(305);
-        prgBarActual.setProgress(0.6);
-        prgBarActual.setPrefSize(565, 24);
+//        prgBarActual.setLayoutX(14);
+//        prgBarActual.setLayoutY(305);
+//        prgBarActual.setProgress(0.6);
+//        prgBarActual.setPrefSize(565, 24);
 
-        ancPostIt.getChildren().addAll(prgBarDate, prgBarActual);
+        ancPostIt.getChildren().addAll(prgBarDate);
     }
 
     private void setLabelText(int index)
@@ -100,15 +104,15 @@ public class PostItFactory
         delDate.setText(formattedDate);
 
         orderNr.setLayoutX(200);
-        orderNr.setLayoutY(70);
+        orderNr.setLayoutY(20);
         orderNr.setText(productionOrders.get(index).getOrderNumber());
 
         startDate.setLayoutX(120);
-        startDate.setLayoutY(208);
+        startDate.setLayoutY(273);
         startDate.setText(formattedStartDate);
 
         endDate.setLayoutX(462);
-        endDate.setLayoutY(208);
+        endDate.setLayoutY(273);
         endDate.setText(formattedEndDate);
 
         String previousDepartment = getPreviousDepartment();
@@ -123,7 +127,7 @@ public class PostItFactory
 
     private String getPreviousDepartment()
     {
-        String previousDepartment = "First task";
+        String previousDepartment = "";
         if (departmentIndex() > 0)
         {
             previousDepartment = productionOrders.get(index).getDeptTasks().get(departmentIndex() - 1).getDepartmentName();
@@ -163,15 +167,16 @@ public class PostItFactory
         Label delDatelabel = new Label("Delivery Date:");
         Label startDatelabel = new Label("Start Date:");
         Label endDatelabel = new Label("End Date:");
-        Label actualTimelabel = new Label("Actual Time:");
-        Label lastActivelabel = new Label("Last Department Active:");
+//        Label actualTimelabel = new Label("Actual Time:");
+//        Label lastActivelabel = new Label("Last Department Active:");
+
 
         customerLabel.setLayoutX(14);
         customerLabel.setLayoutY(120);
         customerLabel.getStyleClass().add("label-sub-header");
 
         orderLabel.setLayoutX(14);
-        orderLabel.setLayoutY(65);
+        orderLabel.setLayoutY(20);
         orderLabel.getStyleClass().add("label-header");
 
         delDatelabel.setLayoutX(14);
@@ -179,22 +184,24 @@ public class PostItFactory
         delDatelabel.getStyleClass().add("label-sub-header");
 
         startDatelabel.setLayoutX(14);
-        startDatelabel.setLayoutY(208);
+        startDatelabel.setLayoutY(273);
         startDatelabel.getStyleClass().add("label-sub-header");
 
         endDatelabel.setLayoutX(365);
-        endDatelabel.setLayoutY(208);
+        endDatelabel.setLayoutY(273);
         endDatelabel.getStyleClass().add("label-sub-header");
 
-        actualTimelabel.setLayoutX(14);
-        actualTimelabel.setLayoutY(275);
-        actualTimelabel.getStyleClass().add("label-sub-header");
+//        actualTimelabel.setLayoutX(14);
+//        actualTimelabel.setLayoutY(275);
+//        actualTimelabel.getStyleClass().add("label-sub-header");
 
-        lastActivelabel.setLayoutX(350);
-        lastActivelabel.setLayoutY(65);
-        lastActivelabel.getStyleClass().add("label-sub-header");
+//        lastActivelabel.setLayoutX(350);
+//        lastActivelabel.setLayoutY(65);
+//        lastActivelabel.getStyleClass().add("label-sub-header");
 
-        ancPostIt.getChildren().addAll(customerLabel, orderLabel, delDatelabel, startDatelabel, endDatelabel, actualTimelabel, lastActivelabel);
+
+
+        ancPostIt.getChildren().addAll(customerLabel, orderLabel, delDatelabel, startDatelabel, endDatelabel);
     }
 
     private void createAnchorPane()
@@ -205,18 +212,18 @@ public class PostItFactory
         flowPane.getChildren().add(ancPostIt);
     }
 
-    private void createComboBox()
-    {
-        ComboBox comboDept = new ComboBox();
-
-        comboDept.setPromptText("Departments");
-        comboDept.setLayoutX(380);
-        comboDept.setLayoutY(14);
-        comboDept.setPrefSize(200, 15);
-
-        ancPostIt.getChildren().add(comboDept);
-
-    }
+//    private void createComboBox()
+//    {
+//        ComboBox comboDept = new ComboBox();
+//
+//        comboDept.setPromptText("Departments");
+//        comboDept.setLayoutX(380);
+//        comboDept.setLayoutY(14);
+//        comboDept.setPrefSize(200, 15);
+//
+//        ancPostIt.getChildren().add(comboDept);
+//
+//    }
 
     private void createButton(int index)
     {
@@ -242,5 +249,55 @@ public class PostItFactory
             }
         });
         ancPostIt.getChildren().add(doneButton);
+    }
+
+    private void createVBox()
+    {
+        VBox vbox = new VBox();
+        
+        vbox.setLayoutX(478);
+        vbox.setLayoutY(20);
+        vbox.setAlignment(Pos.CENTER_RIGHT);
+//        vbox.setSpacing(5);
+        
+        Label dep1 = new Label("Halvfab");
+        Label dep2 = new Label("Bælg");
+        Label dep3 = new Label("Montage 1");
+        Label dep4 = new Label("Montage 2");
+        Label dep5 = new Label("Bertel");
+        Label dep6 = new Label("Maler");
+        Label dep7 = new Label("Forsendelse");
+        
+        dep1.setLayoutX(505);
+        dep1.setLayoutY(20);
+        dep1.getStyleClass().add("label-dep");
+        
+        dep2.setLayoutX(525);
+        dep2.setLayoutY(60);
+        dep2.getStyleClass().add("label-dep");
+        
+        dep3.setLayoutX(478);
+        dep3.setLayoutY(100);
+        dep3.getStyleClass().add("label-dep");
+        
+        dep4.setLayoutX(480);
+        dep4.setLayoutY(140);
+        dep4.getStyleClass().add("label-dep");
+        
+        dep5.setLayoutX(500);
+        dep5.setLayoutY(180);
+        dep5.getStyleClass().add("label-dep");
+        
+        dep6.setLayoutX(500);
+        dep6.setLayoutY(220);
+        dep6.getStyleClass().add("label-dep");
+        
+        dep7.setLayoutX(500);
+        dep7.setLayoutY(260);
+        dep7.getStyleClass().add("label-dep");
+        
+        vbox.getChildren().addAll(dep1, dep2, dep3, dep4, dep5, dep6, dep7);
+        
+        ancPostIt.getChildren().add(vbox);
     }
 }
