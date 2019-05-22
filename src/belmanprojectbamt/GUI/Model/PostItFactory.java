e5a4af92051b2bed27847d3351ac53022d86946b
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-05-22 11:57:34 +0200

    Update PostItFactory.java

--- src/belmanprojectbamt/GUI/Model/PostItFactory.java
+++ src/belmanprojectbamt/GUI/Model/PostItFactory.java
@@ -8,11 +8,13 @@ package belmanprojectbamt.GUI.Model;
 import belmanprojectbamt.BE.DepartmentTask;
 import belmanprojectbamt.BE.ProductionOrder;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -31,17 +33,17 @@ import javafx.scene.paint.Color;
  */
 public class PostItFactory
 {
-
+    
     private FlowPane flowPane;
     private AnchorPane ancPostIt;
-
+    
     private List<ProductionOrder> productionOrders;
     private int index;
     private DateFormatter dFormat;
     private String currentDept;
     private int timeOffset;
     private final BelmanModel belModelInstance;
-
+    
     public PostItFactory(FlowPane flowPane, List<ProductionOrder> productionOrders) throws IOException
     {
         belModelInstance = BelmanModel.getInstance();
@@ -52,7 +54,7 @@ public class PostItFactory
         this.currentDept = belModelInstance.getDepartmentName();
         this.timeOffset = belModelInstance.getOffSet();
     }
-
+    
     public void createPostIt()
     {
         if (checkIfPostItReady())
@@ -69,18 +71,18 @@ public class PostItFactory
 
 //        return ancPostIt;
     }
-    
+
     /**
      * Gets Department tasks from production order
-     * 
+     *
      * @return DepartmentTask
      */
     private DepartmentTask getDeptTask()
     {
-      return productionOrders.get(index).getDeptTasks().get(departmentIndex());
+        return productionOrders.get(index).getDeptTasks().get(departmentIndex());
     }
     
-    private boolean getCurrentDate()
+    private boolean isStartDateReached()
     {
         Calendar cal = Calendar.getInstance();
         Long timeOffsetInMilli = TimeUnit.DAYS.toMillis(timeOffset);
@@ -89,10 +91,18 @@ public class PostItFactory
         return curDateMilli >= (getDeptTask().getStartDate().getTime() + timeOffsetInMilli);
     }
     
+    private boolean isEndDateReached()
+    {
+      Calendar cal = Calendar.getInstance();
+      Long curDateMilli = cal.getTimeInMillis();
+      
+      return curDateMilli > getDeptTask().getEndDate().getTime();
+    }
+    
     private boolean checkIfPostItReady()
     {
-        return getCurrentDate() &&
-                getDeptTask().getDepartmentName().toLowerCase().equals(currentDept.toLowerCase());
+        return isStartDateReached()
+                && getDeptTask().getDepartmentName().toLowerCase().equals(currentDept.toLowerCase());
     }
 
     /**
@@ -101,19 +111,19 @@ public class PostItFactory
     private void setProgressBar()
     {
         ProgressBar prgBarDate = new ProgressBar();
-
+        
         prgBarDate.setLayoutX(14);
         prgBarDate.setLayoutY(260);
         prgBarDate.setProgress(0.4);
         prgBarDate.setPrefSize(562, 24);
-
+        
         ancPostIt.getChildren().addAll(prgBarDate);
     }
 
     /**
      * Retrieves information and sets it on labels.
-     * 
-     * @param index 
+     *
+     * @param index
      */
     private void setLabelText(int index)
     {
@@ -122,63 +132,33 @@ public class PostItFactory
         Label orderNr = new Label();
         Label startDate = new Label();
         Label endDate = new Label();
-        Label departmentlabel = new Label();
-
+        
         customerName.setLayoutX(120);
         customerName.setLayoutY(120);
         customerName.setText(productionOrders.get(index).getCustomerName());
-
+        
         String formattedDate = dFormat.formatDate(productionOrders.get(index).getDeliveryDate());
-
+        
         String formattedStartDate = dFormat.formatDate(productionOrders.get(index).getDeptTasks().get(departmentIndex()).getStartDate());
         String formattedEndDate = dFormat.formatDate(productionOrders.get(index).getDeptTasks().get(departmentIndex()).getEndDate());
-
+        
         delDate.setLayoutX(155);
         delDate.setLayoutY(160);
         delDate.setText(formattedDate);
-
+        
         orderNr.setLayoutX(200);
         orderNr.setLayoutY(20);
         orderNr.setText(productionOrders.get(index).getOrderNumber());
-
+        
         startDate.setLayoutX(120);
         startDate.setLayoutY(220);
         startDate.setText(formattedStartDate);
-
+        
         endDate.setLayoutX(462);
         endDate.setLayoutY(220);
         endDate.setText(formattedEndDate);
-
-        String previousDepartment = getPreviousDepartment();
-
-        departmentlabel.setLayoutX(350);
-        departmentlabel.setLayoutY(85);
-        departmentlabel.getStyleClass().add("label");
-        departmentlabel.setText(previousDepartment);
-
-        ancPostIt.getChildren().addAll(customerName, delDate, orderNr, startDate, endDate, departmentlabel);
-    }
-
-    /**
-     * 
-     * @return 
-     */
-    private String getPreviousDepartment()
-    {
-        String previousDepartment = "";
-        if (departmentIndex() > 0)
-        {
-            previousDepartment = productionOrders.get(index).getDeptTasks().get(departmentIndex() - 1).getDepartmentName();
-            if (productionOrders.get(index).getDeptTasks().get(departmentIndex()).isFinishedTask())
-            {
-                Label isDone = new Label("Done");
-                isDone.setLayoutX(360);
-                isDone.setLayoutY(85);
-                isDone.getStyleClass().add("label-done");
-                isDone.setTextFill(Color.web("#33ff33"));
-            }
-        }
-        return previousDepartment;
+        
+        ancPostIt.getChildren().addAll(customerName, delDate, orderNr, startDate, endDate);
     }
 
     private int departmentIndex()
@@ -208,30 +188,30 @@ public class PostItFactory
         Label delDatelabel = new Label("Delivery Date:");
         Label startDatelabel = new Label("Start Date:");
         Label endDatelabel = new Label("End Date:");
-
+        
         customerLabel.setLayoutX(14);
         customerLabel.setLayoutY(120);
         customerLabel.getStyleClass().add("label-sub-header");
-
+        
         orderLabel.setLayoutX(14);
         orderLabel.setLayoutY(20);
         orderLabel.getStyleClass().add("label-header");
-
+        
         delDatelabel.setLayoutX(14);
         delDatelabel.setLayoutY(160);
         delDatelabel.getStyleClass().add("label-sub-header");
-
+        
         startDatelabel.setLayoutX(14);
         startDatelabel.setLayoutY(220);
         startDatelabel.getStyleClass().add("label-sub-header");
-
+        
         endDatelabel.setLayoutX(365);
         endDatelabel.setLayoutY(220);
         endDatelabel.getStyleClass().add("label-sub-header");
-
+        
         ancPostIt.getChildren().addAll(customerLabel, orderLabel, delDatelabel, startDatelabel, endDatelabel);
     }
-    
+
     /**
      * Creates anchorPane.
      */
@@ -239,13 +219,14 @@ public class PostItFactory
     {
         ancPostIt.setPrefSize(570, 410);
         ancPostIt.getStyleClass().add("anchorpane");
-
+        
         flowPane.getChildren().add(ancPostIt);
     }
-    
+
     /**
      * Creates done button with confirmation message.
-     * @param postItIndex 
+     *
+     * @param postItIndex
      */
     private void createButton(int postItIndex)
     {
@@ -270,19 +251,19 @@ public class PostItFactory
         });
         ancPostIt.getChildren().add(doneButton);
     }
-    
+
     /**
-     * Creates HBox with department labels
-     * Sets the colour for department progress.
+     * Creates HBox with department labels Sets the colour for department
+     * progress.
      */
     private void createHBox()
     {
         HBox hbox = new HBox();
-
+        
         hbox.setLayoutX(14);
         hbox.setLayoutY(305);
         hbox.setAlignment(Pos.CENTER_RIGHT);
-
+        
         Label dep1 = new Label("Halvfab");
         Label dep2 = new Label("Bælg");
         Label dep3 = new Label("Montage 1");
@@ -290,48 +271,75 @@ public class PostItFactory
         Label dep5 = new Label("Bertel");
         Label dep6 = new Label("Maler");
         Label dep7 = new Label("Forsendelse");
-
+        
         dep1.setLayoutX(505);
         dep1.setLayoutY(20);
         dep1.setPadding(new Insets(20));
         dep1.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
         dep1.getStyleClass().add("label-dep");
-
+        
         dep2.setLayoutX(525);
         dep2.setLayoutY(60);
         dep2.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
         dep2.getStyleClass().add("label-dep");
-
+        
         dep3.setLayoutX(478);
         dep3.setLayoutY(100);
         dep3.setBackground(new Background(new BackgroundFill(Color.web("#ffcc00"), CornerRadii.EMPTY, Insets.EMPTY)));
         dep3.getStyleClass().add("label-dep");
-
+        
         dep4.setLayoutX(480);
         dep4.setLayoutY(140);
         dep4.getStyleClass().add("label-dep");
-
+        
         dep5.setLayoutX(500);
         dep5.setLayoutY(180);
         dep5.setBackground(new Background(new BackgroundFill(Color.GREY, CornerRadii.EMPTY, Insets.EMPTY)));
         dep5.getStyleClass().add("label-dep");
-
+        
         dep6.setLayoutX(500);
         dep6.setLayoutY(220);
         dep6.getStyleClass().add("label-dep");
-
+        
         dep7.setLayoutX(500);
         dep7.setLayoutY(260);
         dep7.getStyleClass().add("label-dep");
         
         if (getDeptTask().getStartDate().before(getDeptTask().getEndDate()))
-            {
-                dep1.setBackground(new Background(new BackgroundFill(Color.web("#ffcc00"), CornerRadii.EMPTY, Insets.EMPTY)));
-            }
+        {
+            dep1.setBackground(new Background(new BackgroundFill(Color.web("#ffcc00"), CornerRadii.EMPTY, Insets.EMPTY)));
+        }
         
-
         hbox.getChildren().addAll(dep1, dep2, dep3, dep4, dep5, dep6, dep7);
-
+       
         ancPostIt.getChildren().add(hbox);
+        setDeptStatus(hbox);
+    }
+
+    private void setDeptStatus(HBox hbox)
+    {
+        List<Node> nodes = hbox.getChildren();
+        
+        for (Node node : nodes)
+        {
+            if (getDeptTask().isFinishedTask() && getDeptTask().getDepartmentName().toLowerCase().equals(currentDept.toLowerCase()))
+            {
+                
+                node.getStyleClass().add("label-done");
+            }
+            else if (!getDeptTask().isFinishedTask() && getDeptTask().getEndDate().getTime() < System.currentTimeMillis())
+            {
+                node.getStyleClass().add("label-late");
+            }
+            else if (false)
+            {
+               node.getStyleClass().add("label-irrelevant");
+            }
+            else if (isStartDateReached() && !getDeptTask().isFinishedTask())
+            {
+               node.getStyleClass().add("label-ongoing");
+            }
+            node.getStyleClass().add("label-header");
+        }
     }
 }
